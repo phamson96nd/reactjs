3e878707903aab21e97ed36fe3c59aaedf675eae
son.pham <son.pham@kiaisoft.com>
2023-07-18 14:57:36 +0700

    add client and server Note

--- Note/client/src/components/PushNotification.jsx
+++ Note/client/src/components/PushNotification.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { createClient } from 'graphql-ws';
+import { GRAPHQL_SUBSCRIPTION_ENDPOINT } from '../utils/contants.js';
 import { Badge, Menu, MenuItem } from '@mui/material';
 
 const client = createClient({
-  url: 'ws://localhost:4000/graphql'
+  url: GRAPHQL_SUBSCRIPTION_ENDPOINT,
 });
 
 const query = `subscription PushNotification {
@@ -14,21 +15,63 @@ const query = `subscription PushNotification {
 }`;
 
 export default function PushNotification() {
+  const [invisible, setInvisible] = useState(true);
+  const [notification, setNotification] = useState('');
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const open = Boolean(anchorEl);
+  const handleClose = () => {
+    setAnchorEl(null);
+    setNotification('');
+    setInvisible(true);
+  };
+
+  const handleClick = (e) => {
+    if (notification) {
+      setAnchorEl(e.currentTarget);
+    }
+  };
+
   useEffect(() => {
     (async () => {
-      const subscription = client.iterate({
-        query
+      const onNext = (data) => {
+        setInvisible(false);
+
+        const message = data?.data?.notification?.message;
+        setNotification(message);
+        console.log('[PUSH NOTIFICATION]', { data });
+      };
+
+      await new Promise((resolve, reject) => {
+        client.subscribe(
+          {
+            query,
+          },
+          {
+            next: onNext,
+            error: reject,
+            complete: resolve,
+          }
+        );
       });
-
-      for await (const event of subscription) {
-        expect(event).toEqual({ greetings: 'Hi' });
-
-        break;
-      }
     })();
   }, []);
 
   return (
-    <NotificationsIcon />
-  )
+    <>
+      <Badge
+        color='error'
+        variant='dot'
+        invisible={invisible}
+        overlap='circular'
+        sx={{ '&:hover': { cursor: 'pointer' }, ml: '5px' }}
+      >
+        <NotificationsIcon onClick={handleClick} sx={{ color: '#7D9D9C' }} />
+      </Badge>
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>{notification}</MenuItem>
+      </Menu>
+    </>
+  );
 }
